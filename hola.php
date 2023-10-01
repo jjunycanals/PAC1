03c0743ca273bdd4e4fd4465a22c9f7550c74c31
Jordi Juny <jjunyc@uoc.edu>
2023-10-01 12:59:15 +0200

    afegir comentari al commit existent

--- hola.php
+++ hola.php
@@ -1,4 +1,6 @@
 <?php
+// Este comentario está mal y hay que borrarlo
+// añadimos algo para modificar un commit existente
 // El nombre por defecto es Mundo
 echo "Hola Mundo\n";
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
