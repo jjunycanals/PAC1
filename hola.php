2779bdb222cbcdae738a8be9e7035d5e28fad9ec
Jordi Juny <jjunyc@uoc.edu>
2023-10-08 12:23:34 +0200

    Añadimos classe holamundo y hola requiere de holamundo

--- hola.php
+++ hola.php
@@ -2,9 +2,14 @@
 // Este comentario está mal y hay que borrarlo
 // añadimos algo para modificar un commit existente
 // El nombre por defecto es Mundo
+
+require('HolaMundo.php');
+
 echo "Hola Mundo\n";
+
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";
+print new HolaMundo($nombre);
+
 echo "lo que sea";
 echo "next commit";
 echo "next commit next";
