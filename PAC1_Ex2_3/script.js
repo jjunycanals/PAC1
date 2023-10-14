f84494a8750e4401763b40b86a4b96216667f77a
Jordi Juny <jjunyc@uoc.edu>
2023-10-14 16:09:27 +0200

    Ex2_3 acabat al 100%

--- PAC1_Ex2_3/script.js
+++ PAC1_Ex2_3/script.js
@@ -3,6 +3,13 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+// seleccionem el text de les pelis
+const films = document.querySelectorAll('.film');
+
+// Agafem el valor de la moneda seleccionada per l'usuari:
+const currency = document.getElementById('currency-one');
+// Per si l'api dona algun error poder mostrar-ho a l'usuari
+const apiresult = document.getElementById('apiresult');
 
 let ticketPrice = movieSelect.value;
 
@@ -16,17 +23,20 @@ function setMovieData(movieIndex, moviePrice) {
 // Update total an count at the end of the DOM
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    // Copy selected seats in arr
-    //  Map through array
-    // return a new array indexes
+    // Copy selected seats in arr + Map through array + return a new array indexes
     const seatsIndex = [...selectedSeats].map(function(seat) {
         return [...seats].indexOf(seat);
     });
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
     
     const selectedSeatsCount = selectedSeats.length;
+
+    // Per completar l'exercici guardem al localstorage el num de seients per calcular el preu després en funció de la moneda i no haver de crear més codi/funcions
+    localStorage.setItem('numOfSeats', JSON.stringify(selectedSeatsCount));
     count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
+    // total.innerText = selectedSeatsCount * ticketPrice;
+    // Comento la línea anterior per clacular el preu en funció de la moneda
+    calculate();
 }
 
 // Get data from localstorage and populate UI
@@ -37,10 +47,12 @@ function populateUI() {
         seats.forEach((seat, index) => {
             if (selectedSeats.indexOf(index) > -1) {
                 seat.classList.add('selected');
-            }
+            } 
         });
     }
     const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    count.innerText = localStorage.getItem('numOfSeats');
+    total.innerText = localStorage.getItem('numOfSeats') * localStorage.getItem('selectedMoviePrice');
     if (selectedMovieIndex !== null) {
         movieSelect.selectedIndex = selectedMovieIndex;
     }
@@ -61,4 +73,40 @@ container.addEventListener('click', function(e) {
     }
 });
 
+const calculate = async () => {
+    const one = currency.value;
+    // fem la crida de la consulta, es necessari un try/catch
+    try {
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/99e6ff88606075edbf61c0ef/latest/${one}`)
+        // fem la conversió a json si l'estat es 200 que és l'estat d'èxit.
+        if (response.status === 200) {
+            const datos = await response.json();
+            const cambio = datos.conversion_rates["USD"];
+            // Calculo el preu de la película seleccionada en el pick a movie.
+            const precioCambiado = movieSelect.value * cambio;
+            // Actualitzo el localstorage amb el nou preu en funció de la moneda seleccionada
+            localStorage.setItem('selectedMoviePrice', precioCambiado);
+            // cambio el preu de totes les películes en el desplegable de pick a movie
+            films.forEach(function (item, index, arr) {
+                // Cada option li poso dinamicament els valors convertits dels preus i monedas
+                arr[index].innerHTML = "<option class='film' value="+ arr[index].value*cambio +" title='"+ arr[index].title +"'>" + arr[index].title + " ("+ arr[index].value*cambio +" "+ one +")</option>"; 
+            });
+            // Obtinc el num de seients del localstorage
+            const selectedSeats = localStorage.getItem('selectedSeats');
+            const sillas = localStorage.getItem('numOfSeats');
+            const totalfinalconversion = sillas * precioCambiado;
+            total.innerText = `${totalfinalconversion}`;            
+        } else if (response.status === 404) {
+            apiresult.innerText = `Moneda no encontrada`;
+        } else {
+            apiresult.innerText = `Error en la respuesta de la llamada API. Error num. ${response.status}`;
+        }
+    } catch (error) {
+        apiresult.innerText(error);
+    }
+}
 
+// Change currency
+currency.addEventListener('change', function(e) {
+    calculate();
+});
